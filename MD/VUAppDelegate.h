29c36d24171134b2b5a917c0e04b61e9c55746c4
Boris Bügling <b.buegling@gingco.net>
2012-03-06 09:34:29 +0100

    Initial Commit

---
+++ MD/VUAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VUAppDelegate.h
+//  MD
+//
+//  Created by Boris Bügling on 06.03.12.
+//  Copyright (c) 2012 Gingco.Net New Media GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VUAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MD/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MD
+//
+//  Created by Boris Bügling on 06.03.12.
+//  Copyright (c) 2012 Gingco.Net New Media GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "VUAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([VUAppDelegate class]));
+    }
+}
